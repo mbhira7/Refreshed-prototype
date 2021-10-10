a88e41aabd8c3a4522135604649a627e2d654c36
mbhira7 <54266598+mbhira7@users.noreply.github.com>
2021-10-10 04:14:40 +0200

    changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const memberCard = document.querySelectorAll(".member-card");
 const memberText = document.querySelectorAll(".member-text");
+const quantityMinus = document.querySelector("#quantity-minus");
+const quantityPlus = document.querySelector("#quantity-plus");
+const quantityDisplay = document.querySelector("#quantity-display");
 
 
 for(let i=0; i<memberCard.length; i++){
@@ -11,3 +14,20 @@ for(let i=0; i<memberCard.length; i++){
         memberText[i].style.display = "none";
     }
 }
+
+let quantity = 1;
+
+quantityPlus.onclick = () => {
+    if(quantity >= 0){
+        quantity++
+    }
+    quantityDisplay.innerHTML = quantity
+}
+
+quantityMinus.onclick = () => {
+    if(quantity > 0){
+        quantity--
+    }
+    quantityDisplay.innerHTML = quantity
+}
+
